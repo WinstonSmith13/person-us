da903620fb581b9b5db4cf33bf8527b92b8f6688
WinstonSmith13 <krouk.medy@gmail.com>
2023-03-28 14:55:26 +0200

    feat: working on the autoPlay feature for the carousel.

--- components/landing/sections/carousel/CarouselTemplate.vue
+++ components/landing/sections/carousel/CarouselTemplate.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="carousel">
     <slot :current-slide="currentSlide" />
-
+  
     <!--Navigation-->
-
+  
     <div class="navigate">
       <div class="toggle-page left">
         <LeftCarouselButton
@@ -18,60 +18,96 @@
         />
       </div>
     </div>
+    <div class="pagination">
+      <span
+        @click="goToSlide(index)"
+        class="text-white"
+        v-for="(slide, index) in getSlideCount"
+        :key="index"
+        :class="{active : index + 1 === currentSlide}"
+      />
+    </div>
   </div>
 </template>
+  
+  <script setup>
+  import LeftCarouselButton from '../../../template/buttons/carouselButton/LeftCarouselButton.vue'
+  import RightCarouselButton from '../../../template/buttons/carouselButton/RightCarouselButton.vue'
+  import { ref, onMounted } from 'vue';
+  
+  const currentSlide = ref(1);
+  const getSlideCount = ref(null);
+  const autoPlayEnable = ref(true)
+  
+  const nextSlide = () => {
+      if (currentSlide.value === getSlideCount.value) {
+          currentSlide.value = 0;
+      }
+      currentSlide.value += 1;
+  }
+  
+  const prevSlide = () => {
+      if (currentSlide.value === 1) {
+          currentSlide.value = 1;
+          return;
+      }
+      currentSlide.value -= 1;
+  }
 
-<script setup>
-import LeftCarouselButton from '../../../template/buttons/carouselButton/LeftCarouselButton.vue'
-import RightCarouselButton from '../../../template/buttons/carouselButton/RightCarouselButton.vue'
-import { ref, onMounted } from 'vue';
-
-const currentSlide = ref(1);
-const getSlideCount = ref(null);
-
-const nextSlide = () => {
-    if (currentSlide.value === getSlideCount.value) {
-        currentSlide.value = 0;
-    }
-    currentSlide.value += 1;
-}
-
-const prevSlide = () => {
-    if(currentSlide.value === 1) {
-        currentSlide.value = 1;
-        return;
-    }
-    currentSlide.value -= 1;
-}
-
-onMounted(() => {
-    getSlideCount.value = document.querySelectorAll('.slide').length;
-    console.log(getSlideCount.value);
-})
-
-</script>
-
-<style lang="scss" scoped>
-.carousel {
+  const goToSlide = (index) => {
+    currentSlide.value = index +1 ;
+  }
+  
+  onMounted(() => {
+      getSlideCount.value = document.querySelectorAll('.slide').length;
+  })
+  
+  </script>
+  
+  <style lang="scss" scoped>
+  .carousel {
     position: relative;
+  }
+  
+  .navigate {
+  padding: 0 16px;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-
-.navigate {
+  
+  .pagination {
     position: absolute;
-    bottom: 0;
-    right: 0;
+    bottom: 110px;
+    width: 100%;
     display: flex;
-    align-items: center;
-    margin: 16px;
-}
+    gap:16px;
+    justify-content: center;
+    align-items: flex-start;
+  }
 
-.toggle-page {
+  span {
+    cursor: pointer;
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    background-color: #fff;
+    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+  }
+  
+  .active {
+    background-color: #780F89;
+  }
+
+  .toggle-page {
     display: flex;
-}
-
-.right {
-    margin-left: 16px;
+    flex: 1;
+  }
+  
+  .right {
     justify-content: flex-end;
-}
-</style>
-
+  }
+  </style>
