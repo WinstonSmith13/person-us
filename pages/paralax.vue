3305cebf18b7e6f717af98e8ee26d36b477df82c
WinstonSmith13 <krouk.medy@gmail.com>
2023-04-25 15:13:36 +0200

    working on the html part.

--- pages/paralax.vue
+++ pages/paralax.vue
@@ -1,13 +1,38 @@
 <template>
-    <div>
+  <div>
+    <section> 
+      <img
+        src="./../public/images/paralax/bg.jpg"
+        id="bg"
+        alt="bg"
+      >
+      <img
+        src="./../public/images/paralax/moon.png"
+        id="moon"
+        alt="moon"
+      >
+      <img
+        src="./../public/images/paralax/mountain.png"
+        id="mountain"
+        alt="mountain"
+      >
+      <img
+        src="./../public/images/paralax/road.png"
+        id="road"
+        alt="road"
+      >
 
-    </div>
+      <h2 id="text">
+        Moon Ligth
+      </h2>
+    </section>
+  </div>
 </template>
 
 <script setup>
 
 </script>
 
-<style lang="scss" scoped>
+<style>
 
 </style>
